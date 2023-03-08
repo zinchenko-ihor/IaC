41a3e3b8744ca5f3c0b3cbb3a0ab67847cfd552a
zinchenko-ihor <81517545+zinchenko-ihor@users.noreply.github.com>
2023-03-08 12:38:17 +0200

    Update webserver.tf

--- Terraform/webserver.tf
+++ Terraform/webserver.tf
@@ -1,6 +1,15 @@
+variable "aws_access_key" {
+    default = ""
+}
+
+variable "aws_secret_key" {
+    default = ""
+}
+
+
 provider "aws" {
-    access_key = ""
-    secret_key = ""
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
     region = "eu-central-1"
 }
 
